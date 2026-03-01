b819c04408eae750d8c801cbd544981807b43160
JavixVP <112535001+JavixVP@users.noreply.github.com>
2026-02-28 23:52:39 -0600

    Add files via upload

---
+++ P02-423035119.cpp
@@ -0,0 +1,417 @@
+﻿//Práctica 2: índices, mesh, proyecciones, transformaciones geométricas
+#include <stdio.h>
+#include <string.h>
+#include<cmath>
+#include<vector>
+#include <glew.h>
+#include <glfw3.h>
+//glm
+#include<glm.hpp>
+#include<gtc\matrix_transform.hpp>
+#include<gtc\type_ptr.hpp>
+//clases para dar orden y limpieza al código
+#include"Mesh.h"
+#include"Shader.h"
+#include"Window.h"
+//Dimensiones de la ventana
+const float toRadians = 3.14159265f / 180.0; //grados a radianes
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<MeshColor*> meshColorList;   // estas tres lineas se usa una lista la cual no esta limitada a fifo o lifo
+std::vector<Shader>shaderList;
+
+//Vertex Shader
+//shaders nuevos se crearían acá
+static const char* vShader = "shaders/shader.vert";
+static const char* fShader = "shaders/shader.frag";
+static const char* vShaderColor = "shaders/shadercolor.vert";    
+static const char* fShaderColor = "shaders/shadercolor.frag";
+//nuevos colores
+static const char* vShaderRojo = "shaders/shaderRojo.vert";
+static const char* vShaderAzul = "shaders/shaderAzul.vert";
+static const char* vShaderVerde = "shaders/shaderVerde.vert";
+static const char* vShaderCafe = "shaders/shaderCafe.vert";
+static const char* vShaderVerdeClaro = "shaders/shaderVerdeClaro.vert";
+
+
+float angulo = 0.0f;
+
+//color café en RGB : 0.478, 0.255, 0.067
+
+//Pirámide triangular regular
+void CreaPiramide()
+{
+	unsigned int indices[] = {
+		0,1,2,
+		1,3,2,
+		3,0,2,
+		1,0,3
+
+	};
+	GLfloat vertices[] = {
+		-0.5f, -0.5f,0.0f,	//0
+		0.5f,-0.5f,0.0f,	//1
+		0.0f,0.5f, -0.25f,	//2
+		0.0f,-0.5f,-0.5f,	//3
+
+	};
+	Mesh* obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj1);
+}
+
+//Vértices de un cubo
+void CrearCubo()
+{
+	unsigned int cubo_indices[] = {
+		// front
+		0, 1, 2,
+		2, 3, 0,
+		// right
+		1, 5, 6,
+		6, 2, 1,
+		// back
+		7, 6, 5,
+		5, 4, 7,
+		// left
+		4, 0, 3,
+		3, 7, 4,
+		// bottom
+		4, 5, 1,
+		1, 0, 4,
+		// top
+		3, 2, 6,
+		6, 7, 3
+	};
+
+	GLfloat cubo_vertices[] = {
+		// front
+		-0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		// back
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f
+	};
+	Mesh* cubo = new Mesh();
+	cubo->CreateMesh(cubo_vertices, cubo_indices, 24, 36);
+	meshList.push_back(cubo);
+}
+
+void CrearLetrasyFiguras()
+
+{
+	//[0]
+	GLfloat vertices_letras[] = {
+	//X		Y		Z		R		G		B
+
+	// ===== "J" ROSA PASTEL =====
+
+	//figura1
+	-0.9f,	-0.4f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.4f,	-0.4f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.9f,	-0.5f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura2
+	-0.9f,	-0.5f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.4f,	-0.4f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.4f,	-0.5f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura3
+	-0.7f,	-0.5f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.55f,	-0.5f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.55f,	-0.9f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura4
+	-0.7f,	-0.5f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.7f,	-0.9f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.55f,	-0.9f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura5
+	-0.7f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.7f,	-0.9f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.9f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura6
+	-0.9f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.7f,	-0.9f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.9f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura7
+	-0.9f,	-0.7f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.8f,	-0.7f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.9f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura8
+	-0.8f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.8f,	-0.7f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.9f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+
+	// ===== "V" AZUL PASTEL =====
+
+	//figura9
+	-0.9f,	-0.9f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.7f,	-0.9f,	0.0f,	1.0f,	0.7f,	0.8f,
+	-0.9f,	-0.8f,	0.0f,	1.0f,	0.7f,	0.8f,
+
+	//figura10
+	-0.25f,	0.3f,	0.0f,	0.7f,	0.8f,	1.0f,
+	-0.1f,	0.3f,	0.0f,	0.7f,	0.8f,	1.0f,
+	-0.1f,	-0.2f,	0.0f,	0.7f,	0.8f,	1.0f,
+
+	//figura11
+	0.0f,	0.0f,	0.0f,	0.7f,	0.8f,	1.0f,
+	-0.1f,	0.3f,	0.0f,	0.7f,	0.8f,	1.0f,
+	-0.1f,	-0.2f,	0.0f,	0.7f,	0.8f,	1.0f,
+
+	//figura12
+	0.0f,	0.0f,	0.0f,	0.7f,	0.8f,	1.0f,
+	0.1f,	-0.2f,	0.0f,	0.7f,	0.8f,	1.0f,
+	-0.1f,	-0.2f,	0.0f,	0.7f,	0.8f,	1.0f,
+
+	//figura13
+	0.0f,	0.0f,	0.0f,	0.7f,	0.8f,	1.0f,
+	0.1f,	0.3f,	0.0f,	0.7f,	0.8f,	1.0f,
+	0.1f,	-0.2f,	0.0f,	0.7f,	0.8f,	1.0f,
+
+
+	// ===== "P" VERDE PASTEL =====
+
+	//figura14
+	0.25f,	0.3f,	0.0f,	0.7f,	0.8f,	1.0f,
+	0.1f,	0.3f,	0.0f,	0.7f,	0.8f,	1.0f,
+	0.1f,	-0.2f,	0.0f,	0.7f,	0.8f,	1.0f,
+
+	//figura15
+	0.45f,	0.9f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.45f,	0.4f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.4f,	0.0f,	0.7f,	1.0f,	0.7f,
+
+	//figura1
+	0.45f,	0.9f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.9f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.4f,	0.0f,	0.7f,	1.0f,	0.7f,
+
+	//figura17
+	0.8f,	0.9f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.9f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.8f,	0.0f,	0.7f,	1.0f,	0.7f,
+
+	//figura18
+	0.8f,	0.9f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.9f,	0.8f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.8f,	0.0f,	0.7f,	1.0f,	0.7f,
+
+	//figura19
+	0.8f,	0.8f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.9f,	0.8f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.8f,	0.7f,	0.0f,	0.7f,	1.0f,	0.7f,
+
+	//figura20
+	0.9f,	0.8f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.9f,	0.7f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.8f,	0.7f,	0.0f,	0.7f,	1.0f,	0.7f,
+
+	//figura21
+	0.6f,	0.7f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.6f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.9f,	0.7f,	0.0f,	0.7f,	1.0f,	0.7f,
+
+
+	//figura22
+	0.8f,	0.6f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.6f,	0.6f,	0.0f,	0.7f,	1.0f,	0.7f,
+	0.9f,	0.7f,	0.0f,	0.7f,	1.0f,	0.7f,
+	};
+	MeshColor* letras = new MeshColor();
+	letras->CreateMeshColor(vertices_letras, 468);
+	meshColorList.push_back(letras);
+
+
+}
+
+
+void CreateShaders()
+{
+	//shader 1 [0]
+	Shader* shader1 = new Shader(); //shader para usar índices: objetos: cubo y  pirámide
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+
+	//shader 2 [1]
+	Shader* shader2 = new Shader();//shader para usar color como parte del VAO: letras 
+	shader2->CreateFromFiles(vShaderColor, fShaderColor);
+	shaderList.push_back(*shader2);
+
+	//shader rojo [2]
+	Shader* shader3 = new Shader();//shader para usar color como parte del VAO: letras 
+	shader3->CreateFromFiles(vShaderRojo, fShader);
+	shaderList.push_back(*shader3);
+
+	//shader azul [3]
+	Shader* shader4 = new Shader();//shader para usar color como parte del VAO: letras 
+	shader4->CreateFromFiles(vShaderAzul, fShader);
+	shaderList.push_back(*shader4);
+
+	//shader verde [4]
+	Shader* shader5 = new Shader();//shader para usar color como parte del VAO: letras 
+	shader5->CreateFromFiles(vShaderVerde, fShader);
+	shaderList.push_back(*shader5);
+
+	//shader cafe [5]
+	Shader* shader6 = new Shader();//shader para usar color como parte del VAO: letras 
+	shader6->CreateFromFiles(vShaderCafe, fShader);
+	shaderList.push_back(*shader6);
+
+	//shader verde claro [6]
+	Shader* shader7 = new Shader();//shader para usar color como parte del VAO: letras 
+	shader7->CreateFromFiles(vShaderVerdeClaro, fShader);
+	shaderList.push_back(*shader7);
+
+}
+
+
+int main()
+{
+	mainWindow = Window(800, 800);
+	mainWindow.Initialise();
+	CreaPiramide(); //índice 0 en MeshList
+	CrearCubo();//índice 1 en MeshList
+	CrearLetrasyFiguras(); //usa MeshColor, índices en MeshColorList
+	CreateShaders();
+	GLuint uniformProjection = 0;
+	GLuint uniformModel = 0;
+	//Projection: Matriz de Dimensión 4x4 para indicar si vemos en 2D( orthogonal) 
+	glm::mat4 projection = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.1f, 100.0f);
+
+	//Model: Matriz de Dimensión 4x4 en la cual se almacena la multiplicación de las transformaciones geométricas.
+	glm::mat4 model(1.0); //fuera del while se usa para inicializar la matriz con una identidad
+
+	//Loop mientras no se cierra la ventana
+	while (!mainWindow.getShouldClose())
+	{
+		//Recibir eventos del usuario
+		glfwPollEvents();
+		//Limpiar la ventana
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Se agrega limpiar el buffer de profundidad
+
+		//LETRAS
+		uniformModel = shaderList[1].getModelLocation();
+		uniformProjection = shaderList[1].getProjectLocation();
+		shaderList[1].useShader();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshColorList[0]->RenderMeshColor();
+	
+
+		//cubo rojo
+		shaderList[2].useShader();
+		uniformModel = shaderList[2].getModelLocation();
+		uniformProjection = shaderList[2].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -0.3f, -4.0f));
+		model = glm::scale(model, glm::vec3(0.7f, 0.7f, 0.7f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[1]->RenderMesh();
+
+		//pirammide azul
+		shaderList[3].useShader();
+		uniformModel = shaderList[3].getModelLocation();
+		uniformProjection = shaderList[3].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, 0.3f, -4.0f));
+		model = glm::scale(model, glm::vec3(0.7f, 0.5f, 0.7f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[0]->RenderMesh();
+
+		//Cubo verde claro 1
+		shaderList[4].useShader();
+		uniformModel = shaderList[4].getModelLocation();
+		uniformProjection = shaderList[4].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-0.16f, -0.15f, -2.0f));
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[1]->RenderMesh();
+
+		//Cubo verde claro 2
+		shaderList[4].useShader();
+		uniformModel = shaderList[4].getModelLocation();
+		uniformProjection = shaderList[4].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.16f, -0.15f, -2.0f));
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[1]->RenderMesh();
+
+		//Cubo verde claro 3
+		shaderList[4].useShader();
+		uniformModel = shaderList[4].getModelLocation();
+		uniformProjection = shaderList[4].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -0.55f, -2.0f));
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[1]->RenderMesh();
+
+		//Cubo cafe 1
+		shaderList[5].useShader();
+		uniformModel = shaderList[5].getModelLocation();
+		uniformProjection = shaderList[5].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-0.6f, -0.55f, -4.0f));
+		model = glm::scale(model, glm::vec3(0.15f, 0.2f, 0.2f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[1]->RenderMesh();
+
+		//Cubo cafe 2
+		shaderList[5].useShader();
+		uniformModel = shaderList[5].getModelLocation();
+		uniformProjection = shaderList[5].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.6f, -0.55f, -4.0f));
+		model = glm::scale(model, glm::vec3(0.15f, 0.2f, 0.2f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[1]->RenderMesh();
+
+		//pirammide verde 1
+		shaderList[6].useShader();	
+		uniformModel = shaderList[6].getModelLocation();
+		uniformProjection = shaderList[6].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.6f, -0.2f, -4.0f));
+		model = glm::scale(model, glm::vec3(0.3f, 0.5f, 0.3f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[0]->RenderMesh();
+
+		//pirammide verde 2
+		shaderList[6].useShader();
+		uniformModel = shaderList[6].getModelLocation();
+		uniformProjection = shaderList[6].getProjectLocation();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-0.6f, -0.2f, -4.0f));
+		model = glm::scale(model, glm::vec3(0.3f, 0.5f, 0.3f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA y se envían al shader como variables de tipo uniform
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[0]->RenderMesh();
+
+		glUseProgram(0);
+		mainWindow.swapBuffers();
+	}
+	return 0;
+}
